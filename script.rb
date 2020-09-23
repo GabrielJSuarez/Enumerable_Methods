a8045c3e5982a32b8e9000c6d1c47a17b557fd38
Gabriel J. Suarez <ginnlaiho@gmail.com>
2020-09-23 15:35:57 -0500

    Added Each_Index Replica method
    Added my_each_with index method, a replica of each_with in Ruby

--- script.rb
+++ script.rb
@@ -8,5 +8,14 @@ module Enumerable
         end
         array
     end
+    
+    def my_each_with_index(array)
+        index = 0
+        while index < array.length
+          yield(array[index], index)
+          index += 1
+        end
+      array
+    end
 
 end
