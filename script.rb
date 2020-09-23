259eb80e520b6c52c81e14a02b81d74a0ac13aea
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-09-23 17:34:17 -0300

    Add my_each method

--- script.rb
+++ script.rb
@@ -1,5 +1,12 @@
 
 module Enumerable
-
+    def my_each(array)
+        index = 0
+        while index < array.length
+            yield(array[index])
+            index += 1
+        end
+        array
+    end
 
 end
