bd2c2bba30d546d77c7db999ec34af4e5e01efa3
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-09-24 18:05:34 -0300

    Add multiply_els and Rubocop fixes

--- script.rb
+++ script.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
-    var = self.to_a
+    var = to_a
     return to_enum(:my_each) unless block_given?
 
     index = 0
@@ -12,7 +14,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    var = self.to_a
+    var = to_a
     return to_enum(:my_each) unless block_given?
 
     index = 0
@@ -24,7 +26,7 @@ module Enumerable
   end
 
   def my_select
-    var = self.to_a
+    var = to_a
     new_arr = []
     return to_enum(:my_select) unless block_given?
 
@@ -35,75 +37,77 @@ module Enumerable
   end
 
   def my_all?(pattern = nil)
-    var = self.to_a
+    var = to_a
     if block_given? == true
-      var.my_select { |x| yield(x) }.length == var.length ? true : false
-    elsif pattern != nil
+      var.my_select { |x| yield(x) }.length == var.length
+    elsif !pattern.nil?
       if pattern.class == Regexp
-        var.my_select { |x| pattern.match(x) != nil }.length == var.length ? true : false
+        var.my_select { |x| !pattern.match(x).nil? }.length == var.length
       elsif pattern.class == Integer
-        var.my_select { |x| x == pattern}.length == var.length ? true : false
+        var.my_select { |x| x == pattern }.length == var.length
       else
-        var.my_select { |x| x.class == pattern}.length == var.length ? true : false
+        var.my_select { |x| x.class == pattern }.length == var.length
       end
     else
-      var.my_select { |x| x != nil && x != false }.length == var.length ? true : false
+      var.my_select { |x| !x.nil? && x != false }.length == var.length
     end
   end
 
   def my_any?(pattern = nil)
-    var = self.to_a
+    var = to_a
     if block_given? == true
       var.my_select { |x| yield(x) }.length.positive? ? true : false
-    elsif pattern != nil
+    elsif !pattern.nil?
       if pattern.class == Regexp
-        var.my_select { |x| pattern.match(x) != nil }.length.positive? ? true : false
+        var.my_select { |x| !pattern.match(x).nil? }.length.positive? ? true : false
       elsif pattern.class == Integer
         var.my_select { |x| x == pattern}.length.positive? ? true : false
       else
         var.my_select { |x| x.class == pattern}.length.positive? ? true : false
       end
     else
-      var.my_select { |x| x != nil && x != false }.length.positive? ? true : false
+      var.my_select { |x| !x.nil? && x != false }.length.positive? ? true : false
     end
   end
 
   def my_none?(pattern = nil)
-    var = self.to_a
+    var = to_a
     if block_given? == true
       var.my_select { |x| yield(x) }.length.zero? ? true : false
-    elsif pattern != nil
+    elsif !pattern.nil?
       if pattern.class == Regexp
-        var.my_select { |x| pattern.match(x) != nil }.length.zero? ? true : false
+        var.my_select { |x| !pattern.match(x).nil? }.length.zero? ? true : false
       elsif pattern.class == Integer
         var.my_select { |x| x == pattern}.length.zero? ? true : false
       else
         var.my_select { |x| x.class == pattern}.length.zero? ? true : false
       end
     else
-      var.my_select { |x| x != nil && x != false }.length.zero? ? true : false
+      var.my_select { |x| !x.nil? && x != false }.length.zero? ? true : false
     end
   end
 
   def my_count(arg = nil)
-    var = self.to_a
-    if arg == nil
+    var = to_a
+    if arg.nil?
       return var.length unless block_given?
+
       var.my_select { |x| yield(x) }.length
-    else 
+    else
       var.my_select { |x| x == arg }.length
     end
   end
 
-  def my_map( proc = nil)
-    var = self.to_a
+  def my_map(proc = nil)
+    var = to_a
     new_arr = []
-    if proc != nil
+    if !proc.nil?
       var.my_each do |x|
         new_arr.push(proc.call(x))
       end
     else
       return to_enum(:my_map) unless block_given?
+
       var.my_each do |x|
         new_arr.push(yield(x))
       end
@@ -112,57 +116,55 @@ module Enumerable
   end
 
   def my_inject(acc = nil, sym = nil)
-    var = self.to_a
+    var = to_a
 
-    if var.my_all? { |x| x.class == String }
-      count = ''  
-    else
-      count = acc
-    end
+    count = if var.my_all? { |x| x.class == String }
+              ''
+            else
+              acc
+            end
 
-    if acc != nil && sym != nil
+    if !acc.nil? && !sym.nil?
       operation = sym
       var.my_each do |x|
-        count = count.send operation, x   
+        count = count.send operation, x
       end
       count
-    elsif acc != nil && acc.class != Integer
+    elsif !acc.nil? && acc.class != Integer
       count = 0
       operation = acc
       var.my_each do |x|
-        count = count.send operation, x   
+        count = count.send operation, x
       end
       count
     elsif acc.class == Integer
-      return "LocalJumpError (no block given)" unless block_given?
+      return 'LocalJumpError (no block given)' unless block_given?
+
       var.my_each do |x|
         count = yield(count, x)
       end
       count
     else
       if count.class != String
-        return "LocalJumpError (no block given)" unless block_given?
-          count = 0
-          var.my_each do |x|
-            count = yield(count, x)
-          end
+        return 'LocalJumpError (no block given)' unless block_given?
+
+        count = 0
+        var.my_each do |x|
+          count = yield(count, x)
+        end
         count
       else
-        return "LocalJumpError (no block given)" unless block_given?
-          var.my_each do |x|
-            count = yield(count, x)
-          end
+        return 'LocalJumpError (no block given)' unless block_given?
+
+        var.my_each do |x|
+          count = yield(count, x)
+        end
         count
-      end  
+      end
     end
   end
-
-
 end
 
-
-p (1..5).my_inject(4) { |prod, n| prod * n }
-
-
-
-
+def multiply_els(arr)
+  arr.my_inject(1, :*)
+end
